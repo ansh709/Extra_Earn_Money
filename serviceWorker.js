bfd24c0ffa533c18a49297d891f62a4e2f9cacc9
venue <82924899+ansh709@users.noreply.github.com>
2021-05-20 21:36:45 +0530

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,29 +1,6 @@
-var statiMoneyApp = "extra-earn-money-site-v1";
-
-var assets = [
-    "/",
-    "/index.html",
-    "/style.css",
-    "/js/app.js",
-    "/js/header.js",
-    "/js/nav.js",
-    "/js/slider.js",
-    "/images/slide01.jpg",
-    "/images/userDefaulticon.png"
-];
-
-self.addEventListener("install", installEvent => {
-    installEvent.waitUntil(
-        caches.open(statiMoneyApp).then(cache => {
-            cache.addAll(assets);
-        })
-    );
-});
-
-self.addEventListener("fetch", fetchEvent => {
-    fetchEvent.respondWith(
-        caches.match(fetchEvent.request).then(res => {
-            return res || fetch(fetchEvent.request);
-        })
-    );
-});
+var _0x6e25 = ["Azures-Cache-V31-LTR", "https://ansh709.github.io/Extra_Earn_Money/index.html", "install", "skipWaiting", "then", "addAll", "open", "waitUntil", "addEventListener", "fetch", "request", "match", "respondWith", "activate", "claim", "clients"];
+var CACHE_NAME = _0x6e25[0];
+var REQUIRED_FILES = [_0x6e25[1]];
+self[_0x6e25[8]](_0x6e25[2], function(_0x175ax3) { _0x175ax3[_0x6e25[7]](caches[_0x6e25[6]](CACHE_NAME)[_0x6e25[4]](function(_0x175ax4) { return _0x175ax4[_0x6e25[5]](REQUIRED_FILES) })[_0x6e25[4]](function() { return self[_0x6e25[3]]() })) });
+self[_0x6e25[8]](_0x6e25[9], function(_0x175ax3) { _0x175ax3[_0x6e25[12]](caches[_0x6e25[11]](_0x175ax3[_0x6e25[10]])[_0x6e25[4]](function(_0x175ax5) { if (_0x175ax5) { return _0x175ax5 }; return fetch(_0x175ax3[_0x6e25[10]]) })) });
+self[_0x6e25[8]](_0x6e25[13], function(_0x175ax3) { _0x175ax3[_0x6e25[7]](self[_0x6e25[15]][_0x6e25[14]]()) })
