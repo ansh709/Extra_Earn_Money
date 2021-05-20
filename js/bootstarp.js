a0d101c67b9cd21573342ca190b4ba2f09db792e
venue <82924899+ansh709@users.noreply.github.com>
2021-05-20 23:11:55 +0530

    Update bootstarp.js

--- js/bootstarp.js
+++ js/bootstarp.js
@@ -1,13 +1,11 @@
-// Initialize deferredPrompt for use later to show browser install prompt.
-let deferredPrompt;
+   window.addEventListener('load', function() {
 
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent the mini-infobar from appearing on mobile
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI notify the user they can install the PWA
-  showInstallPromotion();
-  // Optionally, send analytics event that PWA install promo was shown.
-  console.log(`'beforeinstallprompt' event was fired.`);
-});
+      navigator.serviceWorker.register('service-worker.js', { scope: './' })
+        .then(function(r) {
+          console.log('registered service worker');
+        })
+        .catch(function(whut) {
+          console.error('uh oh... ');
+          console.error(whut);
+        });
+    });
